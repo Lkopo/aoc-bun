664c9e9a8d6de775387c2144f5b4fc357d2f6525
Eduard Karpiel <karpiel@shoptet.cz>
2024-12-11 12:57:13 +0100

    2024 day 11

---
+++ src/2024/11/11.ts
@@ -0,0 +1,51 @@
+export function parse(input: string) {
+  return input
+    .split(' ')
+    .reduce(
+      (map, value) => appendMapKey(map, value, 1),
+      new Map<string, number>()
+    )
+}
+
+export function partOne(input: ReturnType<typeof parse>) {
+  return countStones(input, 25)
+}
+
+export function partTwo(input: ReturnType<typeof parse>) {
+  return countStones(input, 75)
+}
+
+function countStones(stonesMap: Map<string, number>, blinks: number): number {
+  let total = stonesMap.values().reduce((total, count) => total + count, 0)
+  for (let i = 0; i < blinks; ++i) {
+    const entries = [...stonesMap.entries()]
+    entries.forEach(([value, count]) => {
+      const currentDiff = stonesMap.get(value)! - count
+      currentDiff === 0
+        ? stonesMap.delete(value)
+        : stonesMap.set(value, currentDiff)
+      if (value === '0') {
+        stonesMap.set('1', (stonesMap.get('1') || 0) + count)
+      } else if (value.length % 2 === 0) {
+        const length = value.length / 2
+        const [left, right] = [
+          value.slice(0, length),
+          String(Number(value.slice(length)))
+        ]
+        appendMapKey(stonesMap, left, count)
+        appendMapKey(stonesMap, right, count)
+        total += count
+      } else {
+        const multiplied = String(Number(value) * 2024)
+        appendMapKey(stonesMap, multiplied, count)
+      }
+    })
+  }
+  return total
+}
+
+const appendMapKey = (
+  map: Map<string, number>,
+  key: string,
+  value: number
+): Map<string, number> => map.set(key, (map.get(key) || 0) + value)
