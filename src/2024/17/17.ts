d2f1f9140579b772f7316dd7e90fabe7fb7a4b98
Eduard Karpiel <karpiel@shoptet.cz>
2024-12-17 13:02:43 +0100

    2024 day 17

---
+++ src/2024/17/17.ts
@@ -0,0 +1,84 @@
+export function parse(input: string) {
+  const [registersPart, programPart] = input.split('\n\n')
+  const registers = registersPart!
+    .split('\n')
+    .map(line => line.match(/\d+/)!.map(BigInt)[0])
+  return {
+    a: registers[0]!,
+    b: registers[1]!,
+    c: registers[2]!,
+    program: programPart!.split(' ')[1]!.split(',')!.map(Number)
+  } as Computer
+}
+
+export function partOne(input: ReturnType<typeof parse>) {
+  return execute(input)
+}
+
+export function partTwo(input: ReturnType<typeof parse>) {
+  const expectedResult = input.program.join(',')
+  let initialA = 0n
+  while (true) {
+    input.a = initialA
+    const result = execute(input)
+    if (result === expectedResult) {
+      return initialA
+    }
+    initialA =
+      initialA !== 0n && expectedResult.endsWith(result)
+        ? initialA * 8n
+        : initialA + 1n
+  }
+}
+
+type Computer = { a: bigint; b: bigint; c: bigint; program: number[] }
+
+const execute = (computer: Computer) => {
+  let pointer = 0
+  const output: number[] = []
+  while (computer.program[pointer] !== undefined) {
+    const operand = computer.program[pointer + 1]!
+    const instruction = computer.program[pointer]
+    switch (instruction) {
+      case 0:
+        computer.a /= 2n ** getComboOperand(operand, computer)
+        break
+      case 1:
+        computer.b ^= BigInt(operand)
+        break
+      case 2:
+        computer.b = getComboOperand(operand, computer) % 8n
+        break
+      case 3:
+        pointer = computer.a === 0n ? pointer + 2 : operand
+        break
+      case 4:
+        computer.b ^= computer.c
+        break
+      case 5:
+        output.push(Number(getComboOperand(operand, computer) % 8n))
+        break
+      case 6:
+        computer.b = computer.a / 2n ** getComboOperand(operand, computer)
+        break
+      case 7:
+        computer.c = computer.a / 2n ** getComboOperand(operand, computer)
+    }
+    if (instruction !== 3) {
+      pointer += 2
+    }
+  }
+  return output.join(',')
+}
+
+const getComboOperand = (operand: number, computer: Computer): bigint => {
+  if (operand <= 3) {
+    return BigInt(operand)
+  }
+  const mapping = {
+    4: computer.a,
+    5: computer.b,
+    6: computer.c
+  }
+  return mapping[operand as 4 | 5 | 6]
+}
